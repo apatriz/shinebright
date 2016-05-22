194b9c510e2e16f80b1945bf047f41407d25ebb7
apatriz <patrizio@msu.edu>
2016-05-22 00:32:35 -0700

    added more transition animations

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 
 $(document).ready(function () {
     var options = [
-        {selector: '#work-experience', offset: 500, callback: 'Materialize.showStaggeredList("#work-experience")'}
+        {selector: '#work-experience', offset: 500, callback: 'Materialize.showStaggeredList("#work-experience")'},
+        {selector: '#education', offset: 500, callback: 'Materialize.showStaggeredList("#education")'},
+        {selector: '#skills', offset: 500, callback: 'Materialize.showStaggeredList("#skills")'},
+        {selector: '#contact', offset: 400, callback: 'Materialize.showStaggeredList("#contact")'}
+        
 
         
     ];
